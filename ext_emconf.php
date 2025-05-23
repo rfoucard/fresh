30573dfd90bcaa5c8c00d2bd97a7dff169ade22d
rfoucard <rfoucard@w-seils.com>
2025-05-23 13:35:42 +0200

    Create ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Fresh Backend',
+    'description' => 'Tweaks the backend CSS of TYPO3',
+    'category' => 'system',
+    'state' => 'stable',
+    'author' => 'Rachel Foucards',
+    'author_email' => 'rachel.foucard@typo3.org',
+    'author_company' => '',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '14.0.0-14.0.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
